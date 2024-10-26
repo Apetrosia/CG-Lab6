af9c7558f7a45c6d4f4334c19191a5a92497f49b
AlexanderZemlyak <zemlyaksanya@mail.ru>
2024-10-26 23:01:32 +0300

    Implement cube rendering

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,64 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // comboBox1
             // 
-            this.button1.Location = new System.Drawing.Point(206, 247);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 52);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Задание 1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Нарисовать куб"});
+            this.comboBox1.Location = new System.Drawing.Point(752, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // button2
+            // pictureBox1
             // 
-            this.button2.Location = new System.Drawing.Point(468, 251);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(130, 52);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Задание 2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(726, 251);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(130, 52);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Задание 3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(28, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(706, 514);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientSize = new System.Drawing.Size(963, 601);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,31 +1,345 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace CG_Lab
 {
     public partial class Form1 : Form
     {
+        private enum Operation { DrawCube = 0 }
+
+        Graphics g;
+
+        Pen defaultPen = new Pen(Color.Black, 3);
+
         public Form1()
         {
             InitializeComponent();
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            g = Graphics.FromImage(pictureBox1.Image);
+
+            comboBox1.SelectedIndex = 0;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private PointF ProjectParallel(Vertex v)
         {
-            FormTask1 task1Form = new FormTask1();
-            task1Form.Show();
+            return new PointF(v.X, v.Y);
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void DrawPolyhedron(PolyHedron polyhedron)
         {
-            FormTask2 task2Form = new FormTask2();
-            task2Form.Show();
+            foreach (var face in polyhedron.Faces)
+            {
+                var points = new List<PointF>();
+
+                foreach (var vertexIndex in face.Vertices)
+                {
+                    Vertex v = polyhedron.Vertices[vertexIndex];
+                    points.Add(ProjectParallel(v));
+                }
+
+                // g.FillPolygon(Brushes.Red, points.ToArray());
+
+                g.DrawPolygon(defaultPen, points.ToArray());
+                
+            }
+
+            pictureBox1.Invalidate();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        ~Form1()
         {
-            FormTask3 task2Form = new FormTask3();
-            task2Form.Show();
+            g.Dispose();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case (int)Operation.DrawCube:
+                    DrawPolyhedron(PolyHedron.GetCube()
+                                             .Rotated(20, 20, 0)
+                                             .Scaled(100, 100, 100)
+                                             .Moved(pictureBox1.Width / 2, pictureBox1.Height / 2, 0)
+                                  );
+                    break;
+            }
+        }
+    }
+
+    public class Matrix<T> where T : struct, IConvertible
+    {
+        public T[,] Values { get; }
+
+        public Matrix(T[,] values)
+        {
+            Values = values;
+        }
+
+        public static implicit operator Matrix<T>(T[,] values)
+        {
+            return new Matrix<T>(values);
+        }
+
+        public static implicit operator Vertex(Matrix<T> m)
+        {
+            return new Vertex(Convert.ToSingle(m[0, 0]), Convert.ToSingle(m[0, 1]), Convert.ToSingle(m[0, 2]));
+        }
+
+        public Matrix(Vertex vertex)
+        {
+            Values = new T[1, 4] { 
+                  { 
+                    (T)Convert.ChangeType(vertex.X, typeof(T)),
+                    (T)Convert.ChangeType(vertex.Y, typeof(T)),
+                    (T)Convert.ChangeType(vertex.Z, typeof(T)),
+                    (T)Convert.ChangeType(1, typeof(T)) 
+                  } 
+            };
+        }
+
+        public static implicit operator Matrix<T>(Vertex vertex)
+        {
+            return new Matrix<T>(vertex);
+        }
+
+        public static Matrix<T> operator *(Matrix<T> A, Matrix<T> B)
+        {
+            int rowsA = A.Values.GetLength(0);
+            int colsA = A.Values.GetLength(1);
+            int rowsB = B.Values.GetLength(0);
+            int colsB = B.Values.GetLength(1);
+
+            T[,] result = new T[rowsA, colsB];
+
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int j = 0; j < colsB; j++)
+                {
+                    result[i, j] = default;
+                    for (int k = 0; k < colsA; k++)
+                    {
+                        result[i, j] += (dynamic)A.Values[i, k] * (dynamic)B.Values[k, j];
+                    }
+                }
+            }
+
+            return new Matrix<T>(result);
+        }
+
+        public T this[int row, int column]
+        {
+            get
+            {
+                return Values[row, column];
+            }
+            set
+            {
+                Values[row, column] = value;
+            }
+        }
+
+
+    }
+
+    public struct Vertex
+    {
+        public float X { get; private set; }
+        
+        public float Y { get; private set; }
+
+        public float Z { get; private set; }
+
+        public Vertex(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    }
+
+    public class Face
+    {
+        public List<int> Vertices { get; private set; }
+
+        public Face(List<int> vertices)
+        {
+            Vertices = vertices;
+        }
+    }
+
+    public class PolyHedron
+    {
+        public List<Face> Faces { get; private set; }
+
+        public List<Vertex> Vertices { get; private set; }
+
+        public PolyHedron()
+        {
+            Faces = new List<Face>();
+            Vertices = new List<Vertex>();
+        }
+
+        public PolyHedron(List<Face> faces, List<Vertex> vertices)
+        {
+            Faces = faces;
+            Vertices = vertices;
+        }
+
+        public PolyHedron Scaled(float c1, float c2, float c3)
+        {
+            var newPoly = this.Clone();
+
+            Matrix<float> translationMatrix = new float[4, 4]
+            {
+                { c1, 0,  0,  0 },
+                { 0,  c2, 0,  0 },
+                { 0,  0,  c3, 0 },
+                { 0,  0,  0,  1 }
+            };
+
+            for (int i = 0; i < newPoly.Vertices.Count; i++)
+            {
+                newPoly.Vertices[i] *= translationMatrix;
+            }
+
+            return newPoly;
+        }
+
+        public PolyHedron Moved(float a, float b, float c)
+        {
+            var newPoly = this.Clone();
+
+            Matrix<float> translationMatrix = new float[4, 4]
+            {
+                { 1,  0,  0,  0 },
+                { 0,  1,  0,  0 },
+                { 0,  0,  1,  0 },
+                { a,  b,  c,  1 }
+            };
+
+            for (int i = 0; i < newPoly.Vertices.Count; i++)
+            {
+                newPoly.Vertices[i] *= translationMatrix;
+            }
+
+            return newPoly;
+        }
+
+        public PolyHedron RotatedXAxis(float alpha)
+        {
+            var newPoly = this.Clone();
+
+            double angleRadians = (double)alpha * (Math.PI / 180);
+
+            float cos = (float)Math.Cos(angleRadians);
+            float sin = (float)Math.Sin(angleRadians);
+
+            Matrix<float> translationMatrix = new float[4, 4]
+            {
+                { 1,  0,  0,  0 },
+                { 0,  cos, sin,  0 },
+                { 0,  -sin,  cos,  0 },
+                { 0,  0,  0,  1 }
+            };
+
+            for (int i = 0; i < newPoly.Vertices.Count; i++)
+            {
+                newPoly.Vertices[i] *= translationMatrix;
+            }
+
+            return newPoly;
+        }
+
+        public PolyHedron RotatedYAxis(float alpha)
+        {
+            var newPoly = this.Clone();
+
+            double angleRadians = (double)alpha * (Math.PI / 180);
+
+            float cos = (float)Math.Cos(angleRadians);
+            float sin = (float)Math.Sin(angleRadians);
+
+            Matrix<float> translationMatrix = new float[4, 4]
+            {
+                { cos,  0,  -sin,  0 },
+                { 0,  1, 0,  0 },
+                { sin,  0,  cos,  0 },
+                { 0,  0,  0,  1 }
+            };
+
+            for (int i = 0; i < newPoly.Vertices.Count; i++)
+            {
+                newPoly.Vertices[i] *= translationMatrix;
+            }
+
+            return newPoly;
+        }
+
+        public PolyHedron RotatedZAxis(float alpha)
+        {
+            var newPoly = this.Clone();
+
+            double angleRadians = (double)alpha * (Math.PI / 180);
+
+            float cos = (float)Math.Cos(angleRadians);
+            float sin = (float)Math.Sin(angleRadians);
+
+            Matrix<float> translationMatrix = new float[4, 4]
+            {
+                { cos,  sin,  0,  0 },
+                { -sin, cos, 0,  0 },
+                { 0,  0,  1,  0 },
+                { 0,  0,  0,  1 }
+            };
+
+            for (int i = 0; i < newPoly.Vertices.Count; i++)
+            {
+                newPoly.Vertices[i] *= translationMatrix;
+            }
+
+            return newPoly;
+        }
+
+        public PolyHedron Rotated(float xAngle, float yAngle, float zAngle)
+        {
+            return this.Clone()
+                .RotatedXAxis(xAngle)
+                .RotatedYAxis(yAngle)
+                .RotatedZAxis(zAngle);
+        }
+
+        public static PolyHedron GetCube()
+        {
+            var cube = new PolyHedron();
+
+            cube.Vertices.Add(new Vertex(-1, -1, -1));
+            cube.Vertices.Add(new Vertex(1, -1, -1));
+            cube.Vertices.Add(new Vertex(1, 1, -1));
+            cube.Vertices.Add(new Vertex(-1, 1, -1)); 
+            cube.Vertices.Add(new Vertex(-1, -1, 1));  
+            cube.Vertices.Add(new Vertex(1, -1, 1));  
+            cube.Vertices.Add(new Vertex(1, 1, 1));   
+            cube.Vertices.Add(new Vertex(-1, 1, 1)); 
+
+            cube.Faces.Add(new Face(new List<int> { 0, 1, 2, 3 }));
+            cube.Faces.Add(new Face(new List<int> { 4, 5, 6, 7 })); 
+            cube.Faces.Add(new Face(new List<int> { 0, 1, 5, 4 }));
+            cube.Faces.Add(new Face(new List<int> { 3, 2, 6, 7 })); 
+            cube.Faces.Add(new Face(new List<int> { 1, 2, 6, 5 })); 
+            cube.Faces.Add(new Face(new List<int> { 0, 3, 7, 4 }));
+
+            return cube;
+        }
+
+        public PolyHedron Clone()
+        {
+            var newPoly = new PolyHedron(this.Faces, new List<Vertex>(this.Vertices));
+
+            return newPoly;
         }
     }
 }
