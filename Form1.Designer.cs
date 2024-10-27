8c514317e8b18aa8f568d18f60d392a6ebec80a3
AlexanderZemlyak <zemlyaksanya@mail.ru>
2024-10-27 19:36:06 +0300

    Implement other polyhedrons' rendering and affine transformations

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,14 +30,32 @@
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "Нарисовать куб"});
+            "Отобразить куб",
+            "Отобразить тетраэдр",
+            "Отобразить октаэдр",
+            "Отобразить икосаэдр",
+            "Отобразить додекаэдр"});
             this.comboBox1.Location = new System.Drawing.Point(752, 12);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(180, 21);
@@ -52,18 +70,164 @@
             this.pictureBox1.Size = new System.Drawing.Size(706, 514);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Location = new System.Drawing.Point(752, 258);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Location = new System.Drawing.Point(826, 258);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 2;
+            this.numericUpDown3.Location = new System.Drawing.Point(891, 258);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown3.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Смещение",
+            "Масштабирование",
+            "Поворот"});
+            this.comboBox2.Location = new System.Drawing.Point(790, 216);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(813, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Опорная точка";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 2;
+            this.numericUpDown4.Location = new System.Drawing.Point(749, 355);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown4.TabIndex = 9;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.DecimalPlaces = 2;
+            this.numericUpDown5.Location = new System.Drawing.Point(826, 355);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDown5.TabIndex = 8;
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.DecimalPlaces = 2;
+            this.numericUpDown6.Location = new System.Drawing.Point(891, 355);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown6.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(963, 601);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +235,14 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,16 +2,21 @@
 using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace CG_Lab
 {
     public partial class Form1 : Form
     {
-        private enum Operation { DrawCube = 0 }
+        private enum RenderingOp { DrawCube = 0, DrawTetrahedron, DrawOctahedron, DrawIcosahedron, DrawDodecahedron }
+
+        private enum AffineOp { Move = 0, Scaling, Rotation }
 
         Graphics g;
 
         Pen defaultPen = new Pen(Color.Black, 3);
+
+        PolyHedron currentPolyhedron;
 
         public Form1()
         {
@@ -55,18 +60,90 @@ namespace CG_Lab
             g.Dispose();
         }
 
+        
+
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            g.Clear(pictureBox1.BackColor);
+
             switch (comboBox1.SelectedIndex)
             {
-                case (int)Operation.DrawCube:
-                    DrawPolyhedron(PolyHedron.GetCube()
+                case (int)RenderingOp.DrawCube:
+                    DrawPolyhedron(currentPolyhedron = PolyHedron.GetCube()
                                              .Rotated(20, 20, 0)
                                              .Scaled(100, 100, 100)
                                              .Moved(pictureBox1.Width / 2, pictureBox1.Height / 2, 0)
                                   );
                     break;
+                case (int)RenderingOp.DrawTetrahedron:
+                    DrawPolyhedron(currentPolyhedron = PolyHedron.GetTetrahedron()
+                                             .Rotated(10, 10, 0)
+                                             .Scaled(100, 100, 100)
+                                             .Moved(pictureBox1.Width / 2, pictureBox1.Height / 2, 0)
+                                  );
+                    break;
+                case (int)RenderingOp.DrawOctahedron:
+                    DrawPolyhedron(currentPolyhedron = PolyHedron.GetOctahedron()
+                                             .Rotated(20, 20, 0)
+                                             .Scaled(100, 100, 100)
+                                             .Moved(pictureBox1.Width / 2, pictureBox1.Height / 2, 0)
+                                  );
+                    break;
+                case (int)RenderingOp.DrawIcosahedron:
+                    DrawPolyhedron(currentPolyhedron = PolyHedron.GetIcosahedron()
+                                             .Rotated(10, 10, 0)
+                                             .Scaled(150, 150, 150)
+                                             .Moved(pictureBox1.Width / 2, pictureBox1.Height / 2, 0)
+                                  );
+                    break;
+                case (int)RenderingOp.DrawDodecahedron:
+                    DrawPolyhedron(currentPolyhedron = PolyHedron.GetDodecahedron()
+                                             .Rotated(10, 10, 0)
+                                             .Scaled(200, 200, 200)
+                                             .Moved(pictureBox1.Width / 2, pictureBox1.Height / 2, 0)
+                                  );
+                    break;                   
             }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            g.Clear(pictureBox1.BackColor);
+
+            Vertex anchor;
+
+            switch (comboBox2.SelectedIndex)
+            {
+                case (int)AffineOp.Move:
+                    DrawPolyhedron(currentPolyhedron = currentPolyhedron
+                                                       .Moved((float)numericUpDown1.Value, (float)numericUpDown2.Value, (float)numericUpDown3.Value));
+                    break;
+                case (int)AffineOp.Scaling:
+                    anchor = new Vertex((float)numericUpDown4.Value, (float)numericUpDown5.Value, (float)numericUpDown6.Value);
+
+                    DrawPolyhedron(currentPolyhedron = currentPolyhedron
+                                                       .Moved(-anchor.X, -anchor.Y, -anchor.Z)
+                                                       .Scaled((float)numericUpDown1.Value, (float)numericUpDown2.Value, (float)numericUpDown3.Value)
+                                                       .Moved(anchor.X, anchor.Y, anchor.Z));
+
+                    break;
+
+                case (int)AffineOp.Rotation:
+                    anchor = new Vertex((float)numericUpDown4.Value, (float)numericUpDown5.Value, (float)numericUpDown6.Value);
+
+                    DrawPolyhedron(currentPolyhedron = currentPolyhedron
+                                                       .Moved(-anchor.X, -anchor.Y, -anchor.Z)
+                                                       .Rotated((float)numericUpDown1.Value, (float)numericUpDown2.Value, (float)numericUpDown3.Value)
+                                                       .Moved(anchor.X, anchor.Y, anchor.Z));
+                    break;
+            }
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            numericUpDown4.Value = e.X;
+            numericUpDown5.Value = e.Y;
         }
     }
 
@@ -159,13 +236,22 @@ namespace CG_Lab
             Y = y;
             Z = z;
         }
+
+        public float DistanceTo(in Vertex other)
+        {
+            float xDelta = X - other.X;
+            float yDelta = Y - other.Y;
+            float zDelta = Z - other.Z;
+
+            return (float)Math.Sqrt(xDelta * xDelta + yDelta * yDelta + zDelta * zDelta);
+        }
     }
 
     public class Face
     {
-        public List<int> Vertices { get; private set; }
+        public int[] Vertices { get; private set; }
 
-        public Face(List<int> vertices)
+        public Face(params int[] vertices)
         {
             Vertices = vertices;
         }
@@ -325,14 +411,184 @@ namespace CG_Lab
             cube.Vertices.Add(new Vertex(1, 1, 1));   
             cube.Vertices.Add(new Vertex(-1, 1, 1)); 
 
-            cube.Faces.Add(new Face(new List<int> { 0, 1, 2, 3 }));
-            cube.Faces.Add(new Face(new List<int> { 4, 5, 6, 7 })); 
-            cube.Faces.Add(new Face(new List<int> { 0, 1, 5, 4 }));
-            cube.Faces.Add(new Face(new List<int> { 3, 2, 6, 7 })); 
-            cube.Faces.Add(new Face(new List<int> { 1, 2, 6, 5 })); 
-            cube.Faces.Add(new Face(new List<int> { 0, 3, 7, 4 }));
+            cube.Faces.Add(new Face( 0, 1, 2, 3 ));
+            cube.Faces.Add(new Face( 4, 5, 6, 7 )); 
+            cube.Faces.Add(new Face( 0, 1, 5, 4 ));
+            cube.Faces.Add(new Face( 3, 2, 6, 7 )); 
+            cube.Faces.Add(new Face( 1, 2, 6, 5 )); 
+            cube.Faces.Add(new Face( 0, 3, 7, 4 ));
 
             return cube;
+        }
+
+        public static PolyHedron GetTetrahedron()
+        {
+            var tetra = new PolyHedron();
+
+            tetra.Vertices.Add(new Vertex(-1, 1, -1));
+            tetra.Vertices.Add(new Vertex(1, -1, -1));
+            tetra.Vertices.Add(new Vertex(1, 1, 1));
+            tetra.Vertices.Add(new Vertex(-1, -1, 1));
+
+            tetra.Faces.Add(new Face(0, 1, 2));
+            tetra.Faces.Add(new Face(0, 1, 3));
+            tetra.Faces.Add(new Face(0, 2, 3));
+            tetra.Faces.Add(new Face(1, 2, 3));
+
+            return tetra;
+        }
+
+        public static PolyHedron GetOctahedron()
+        {
+            var cube = GetCube();
+
+            var octa = new PolyHedron();
+
+            foreach (Face face in cube.Faces)
+            {
+                octa.Vertices.Add(cube.GetFaceCenter(face));
+            }
+
+            var octaCenters = cube.Scaled(1 / 3f, 1 / 3f, 1 / 3f).Vertices;
+
+            for (int i = 0; i < 8; i++)
+            {
+                var faceVertices = octa.Vertices.Select((v, ind) => (v, ind))
+                                                .OrderBy(p => octaCenters[i].DistanceTo(in p.v))
+                                                .Select(p => p.ind)
+                                                .Take(3);
+
+                octa.Faces.Add(new Face(faceVertices.ToArray()));
+            }
+
+            return octa;
+        }
+
+        public static PolyHedron GetIcosahedron()
+        {
+            var icosa = new PolyHedron();
+
+            var verticesBottom = new List<(Vertex v, int number)>(5);
+
+            var verticesTop = new List<(Vertex v, int number)>(5);
+
+            double angle = -90;
+
+            int number = 1;
+
+            for (int i = 0; i < 5; i++)
+            {
+                var angleRadians = angle * (Math.PI / 180);
+
+                verticesBottom.Add((new Vertex((float)Math.Cos(angleRadians), -0.5f, (float)Math.Sin(angleRadians)), number));
+
+                angle += 72;
+
+                number += 2;
+            }
+
+            angle = -54;
+
+            number = 2;
+
+            for (int i = 0; i < 5; i++)
+            {
+                var angleRadians = angle * (Math.PI / 180);
+
+                verticesTop.Add((new Vertex((float)Math.Cos(angleRadians), 0.5f, (float)Math.Sin(angleRadians)), number));
+
+                angle += 72;
+
+                number += 2;
+            }
+
+            icosa.Vertices = verticesBottom.Concat(verticesTop).OrderBy(p => p.number).Select(p => p.v).ToList();
+
+            for (int i = 1; i <= 8; i++)
+            {
+                icosa.Faces.Add(new Face(i - 1, i, i + 1));
+            }
+
+            icosa.Faces.Add(new Face(8, 9, 0));
+            icosa.Faces.Add(new Face(9, 0, 1));
+
+            icosa.Vertices.Add(new Vertex(0, -(float)Math.Sqrt(5) / 2, 0));
+            icosa.Vertices.Add(new Vertex(0, (float)Math.Sqrt(5) / 2, 0));
+
+            number = 1;
+
+            for (int i = 0; i < 4; i++)
+            {
+                icosa.Faces.Add(new Face(10, number - 1, number + 1));
+
+                number += 2;
+            }
+
+            icosa.Faces.Add(new Face(10, 8, 0));
+
+            number = 2;
+
+            for (int i = 0; i < 4; i++)
+            {
+                icosa.Faces.Add(new Face(11, number - 1, number + 1));
+
+                number += 2;
+            }
+
+            icosa.Faces.Add(new Face(11, 9, 1));
+
+            return icosa;
+        }
+
+        public static PolyHedron GetDodecahedron()
+        {
+            var icosa = GetIcosahedron();
+
+            var dodeca = new PolyHedron();
+
+            foreach (Face face in icosa.Faces)
+            {
+                dodeca.Vertices.Add(icosa.GetFaceCenter(face));
+            }
+
+            for (int i = 0; i < 12; i++)
+            {
+                var faceVertices = dodeca.Vertices.Select((v, ind) => (v, ind))
+                                                .OrderBy(p => icosa.Vertices[i].DistanceTo(in p.v))
+                                                .Select(p => p.ind)
+                                                .Take(5);
+
+                int first = faceVertices.First();
+
+                var rest = faceVertices.Skip(1).Select(ind => (dodeca.Vertices[ind], ind)).OrderBy(p => dodeca.Vertices[first].DistanceTo(in p.Item1));
+
+                var next = rest.First().ind;
+
+                var lastTwo = rest.Skip(2).OrderBy(p => dodeca.Vertices[next].DistanceTo(in p.Item1));
+
+                dodeca.Faces.Add(new Face(faceVertices.Take(1)
+                                          .Concat(new int[1] { next })
+                                          .Concat(lastTwo.Select(p => p.ind))
+                                          .Concat(rest.Select(p => p.ind).Skip(1).Take(1))
+                                          .ToArray()
+                                 ));
+            }
+
+            return dodeca;
+        }
+
+        public Vertex GetFaceCenter(Face face)
+        {
+            float x = 0, y = 0, z = 0;
+            
+            foreach (int vertexIndex in face.Vertices)
+            {
+                x += Vertices[vertexIndex].X;
+                y += Vertices[vertexIndex].Y;
+                z += Vertices[vertexIndex].Z;
+            }
+            
+            return new Vertex(x / face.Vertices.Length, y / face.Vertices.Length, z / face.Vertices.Length);
         }
 
         public PolyHedron Clone()
